a36f0a4a5fa6fc0937f833a2c4767c4bb30fe96b
iwanmitowski <iwan.mitowski@gmail.com>
2020-12-28 22:30:55 +0200

    Added some exercises

---
+++ SoftUniFundamentalsSept2020/MidExamPreps/FundamentalsMidExamRetake-07.04.2020/01.CounterStrike/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            int wins = 0;
+            int battles = 0;
+            bool isWon = true;
+
+            while (input != "End of battle")
+            {
+                battles++;
+                int distance = int.Parse(input);
+
+                if(energy<distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    isWon = false;
+                    break;
+                }
+
+                energy -= distance;
+                wins++;
+
+
+                if (battles % 3 == 0)
+                {
+                    energy += battles;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (isWon)
+            {
+                Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+            }
+        }
+    }
+}
