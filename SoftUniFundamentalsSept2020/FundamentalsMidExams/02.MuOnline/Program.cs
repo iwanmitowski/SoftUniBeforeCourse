b29bf1d77ed861f68ad451e2ad1fbe4ac9fc7779
iwanmitowski <iwan.mitowski@gmail.com>
2020-12-29 10:30:26 +0200

    Added new exercises.

---
+++ SoftUniFundamentalsSept2020/FundamentalsMidExams/02.MuOnline/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _02.MuOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rooms = Console.ReadLine()
+                .Split(new[] {" ","|"}, StringSplitOptions.RemoveEmptyEntries);
+
+            int health = 100;
+            int bitcoins = 0;
+            int completedRooms = 1;
+            bool isWinner = true;
+
+            for (int i = 0; i < rooms.Length; i+=2)
+            {
+                
+                string action = rooms[i]; //or monster 
+                int value = int.Parse(rooms[i+1]);
+
+                if (action == "potion")
+                {
+                    health += value;
+                    if (health>100)
+                    {
+                        value = 100 - health + value ;
+                        health = 100;
+
+                    }
+                    Console.WriteLine($"You healed for {value} hp.");
+                    Console.WriteLine($"Current health: {health} hp.");
+                }
+                else if (action == "chest")
+                {
+                    bitcoins += value;
+                    Console.WriteLine($"You found {value} bitcoins.");
+                }
+                else
+                {
+                    //monster
+                    health -= value;
+                    if (health>0)
+                    {
+                        Console.WriteLine($"You slayed {action}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {action}.");
+                        Console.WriteLine($"Best room: {completedRooms}");
+                        isWinner = false;
+                        break;
+                    }
+                }
+                completedRooms++;
+
+            }
+            if (isWinner)
+            {
+                Console.WriteLine("You've made it!");
+                Console.WriteLine($"Bitcoins: {bitcoins}");
+                Console.WriteLine($"Health: {health}");
+            }
+        }
+    }
+}
---
+++ SoftUniFundamentalsSept2020/FundamentalsMidExams/03.Inventory/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> journal = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+
+            string[] input = Console.ReadLine()
+                .Split(new[] { " - ", ":" }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "Craft!")
+            {
+                string command = input[0];
+                if (command == "Combine Items")
+                {
+                    string oldItem = input[1];
+                    string newItem = input[2];
+                    if (journal.Contains(oldItem))
+                    {
+                        int indexOldItem = journal.FindIndex(x => x == oldItem);
+                        journal.Insert(indexOldItem + 1, newItem);
+                    }
+                    
+
+                }
+                string item = input[1];
+
+                if (command == "Collect")
+                {
+
+                    if (!journal.Contains(item))
+                    {
+                        journal.Add(item);
+                    }
+                }
+                else if (command == "Drop")
+                {
+                    if (journal.Contains(item))
+                    {
+                        journal.Remove(item);
+                    }
+                }
+                else if (command == "Renew")
+                {
+                    if (journal.Contains(item))
+                    {
+                        journal.Remove(item);
+                        journal.Add(item);
+                    }
+                }
+
+
+
+                input = Console.ReadLine()
+                .Split(new[] { " - ", ":" }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            Console.WriteLine(string.Join(", ", journal));
+        }
+    }
+}
