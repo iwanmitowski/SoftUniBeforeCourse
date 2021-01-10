05abf14e6ecec1083ff459f0398f16448b0e6581
iwanmitowski <iwan.mitowski@gmail.com>
2021-01-10 10:10:53 +0200

    Added exercise

---
+++ SoftUniFundamentalsSept2020/RegularExpressions/05.NetherRealms/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05.NetherRealms
+{
+    class Demon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public double Damage { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Demon> allDemons = new List<Demon>();
+            string patternForHealth = @"[A-z]";
+            string patternForDmg = @"[+-]?[0-9]+\.?[0-9]*";
+            ;
+            Regex regexDamage = new Regex(patternForDmg);
+            string[] demons = Console.ReadLine().Split(new[] {',',' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var d in demons)
+            {
+                string exceptions = "0123456789+-/*.";
+                int health = d.Where(x=>!exceptions.Contains(x)).Sum(c =>c);
+                
+
+                double damage = CalculateDamage(regexDamage, d);
+
+                Demon demon = new Demon();
+                allDemons.Add(new Demon { Name = d, Damage = damage, Health = health });
+            }
+
+            foreach (var demon in allDemons.OrderBy(x=>x.Name))
+            {
+                Console.WriteLine($"{demon.Name} - { demon.Health} health, { demon.Damage:f2} damage");
+            }
+        }
+
+        static double CalculateDamage(Regex regexDamage, string demon)
+        {
+            double damage = 0;
+            MatchCollection nums = regexDamage.Matches(demon);
+
+            foreach (Match match in nums)
+            {
+                damage += double.Parse(match.Value);
+            }
+
+            foreach (var ch in demon)
+            {
+                if (ch == '*')
+                {
+                    damage *= 2.0;
+                }
+                if (ch == '/')
+                {
+                    damage /= 2.0;
+                }
+            }
+
+            return damage;
+        }
+    }
+}
---
+++ SoftUniFundamentalsSept2020/RegularExpressions/06.ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))";
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
