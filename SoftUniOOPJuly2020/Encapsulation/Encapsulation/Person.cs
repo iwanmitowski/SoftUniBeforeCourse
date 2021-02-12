6d9608509b2ae6f6394f0a7a94aaf0af31e45739
iwanmitowski <iwan.mitowski@gmail.com>
2021-02-12 18:07:01 +0200

    Adding new exercises

---
+++ SoftUniOOPJuly2020/Encapsulation/Encapsulation/Person.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (ValidateName(value))
+                {
+                    this.firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (ValidateName(value))
+                {
+                    this.lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if (value>0)
+                {
+                    this.age = value;
+
+                }
+                else
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+        public decimal Salary {
+            get
+            {
+                return this.salary;
+            }
+
+            private set
+            {
+                if (value<460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+
+                }
+                this.salary = value;
+            }
+
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age >= 30)
+            {
+                this.Salary += (this.Salary * percentage) / 100;
+
+            }
+            else
+            {
+                this.Salary += (this.Salary * percentage) / 200;
+
+            }
+
+        }
+        //public override string ToString()
+        //{
+        //    return $"{this.FirstName} receives {this.Salary:f2} leva.";
+        //}
+
+        private bool ValidateName(string name)
+        {
+            if (name.Length > 2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SoftUniOOPJuly2020/Encapsulation/Encapsulation/Team.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam { get=> this.firstTeam.AsReadOnly();}
+        public IReadOnlyCollection<Person> ReserveTeam { get => this.reserveTeam.AsReadOnly();}
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age<40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"First team has {this.firstTeam.Count} players");
+            sb.AppendLine($"Reserve team has {this.reserveTeam.Count} players");
+            return sb.ToString();
+        }
+    }
+}
