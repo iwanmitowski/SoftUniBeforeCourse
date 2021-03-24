53e1ccb38fc4d2bbb18a25798a9f468c931addc5
iwanmitowski <iwan.mitowski@gmail.com>
2021-03-24 14:57:21 +0200

    Adding the last 2 exercises

---
+++ SoftUniAlgorithmsJuly2019/Recursion/06.8QueensPuzzle/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._8QueensPuzzle
+{
+    class Program
+    {
+        static bool[,] board = new bool[8, 8];
+        static HashSet<int> attackedRows = new HashSet<int>();
+        static HashSet<int> attackedCols = new HashSet<int>();
+        static HashSet<int> attackedMainDiagonals = new HashSet<int>();
+        static HashSet<int> attackedSecondaryDiagonals = new HashSet<int>();
+
+        static void Main(string[] args)
+        {
+            Solve(0);
+        }
+
+        private static void Solve(int row)
+        {
+            if (row == 8)
+            {
+                Print();
+                return;
+            }
+
+            for (int col = 0; col < 8; col++)
+            {
+                if (!IsViableCell(row,col))
+                {
+                    MarkAllAttacked(row, col);
+                    Solve(row + 1);
+                    UnMarkAllAttacked(row, col);
+                }
+            }
+        }
+
+        private static void UnMarkAllAttacked(int row, int col)
+        {
+            board[row, col] = false;
+            attackedRows.Remove(row);
+            attackedCols.Remove(col);
+            attackedMainDiagonals.Remove(col - row);
+            attackedSecondaryDiagonals.Remove(col + row);
+
+        }
+
+        private static void MarkAllAttacked(int row, int col)
+        {
+            board[row, col] = true;
+            attackedRows.Add(row);
+            attackedCols.Add(col);
+            attackedMainDiagonals.Add(col - row);
+            attackedSecondaryDiagonals.Add(col + row);
+
+        }
+
+        private static bool IsViableCell(int row, int col)
+        {
+            return attackedRows.Contains(row) ||
+                   attackedCols.Contains(col) ||
+                   attackedMainDiagonals.Contains(col - row) ||
+                   attackedSecondaryDiagonals.Contains(col + row);
+        }
+
+        private static void Print()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (board[i, j] == true)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SoftUniAlgorithmsJuly2019/Recursion/07.PathsInLabyrinth/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PathsInLabyrinth
+{
+    class Program
+    {
+        static char[,] field;
+        static List<char> path = new List<char>();
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            field = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string col = Console.ReadLine();
+                for (int j = 0; j < cols; j++)
+                {
+                    field[i, j] = col[j];
+                }
+            }
+
+            Solve(0, 0, 'S');
+        }
+
+        private static void Solve(int row, int col, char direction)
+        {
+            if (CellIsViable(row,col)==false)
+            {
+                return;
+            }
+
+            path.Add(direction);
+
+            if (field[row, col] == 'e')
+            {
+                Console.WriteLine(string.Join("", path.Skip(1)));
+                               
+            }
+            else if (!IsVisited(row, col)&&IsFree(row,col))
+            {
+                Mark(row, col);
+                Solve(row, col + 1, 'R');
+                Solve(row, col - 1, 'L');
+                Solve(row + 1, col, 'D');
+                Solve(row - 1, col, 'U');
+                UnMark(row, col);
+
+            }
+
+            path.RemoveAt(path.Count - 1);
+
+        }
+
+        private static bool IsFree(int row, int col)
+        {
+            return field[row, col] == '-';
+        }
+
+        private static bool IsVisited(int row, int col)
+        {
+            if (field[row,col]=='v')
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void UnMark(int row, int col)
+        {
+            field[row, col] = '-';
+
+        }
+
+        private static void Mark(int row, int col)
+        {
+            field[row, col] = 'v';
+        }
+
+        private static bool CellIsViable(int row, int col)
+        {
+            return row >= 0 && row < field.GetLength(0) &&
+                col >= 0 && col < field.GetLength(1) &&
+                field[row, col] != '*'&&
+                field[row, col] != 'v'
+                ;
+        }
+    }
+}
