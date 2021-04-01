fa86702ad65877f862d50733cc000a29507a1d3b
iwanmitowski <iwan.mitowski@gmail.com>
2021-04-01 17:15:28 +0300

    Adding the last two exercises

---
+++ SoftUniAlgorithmsJuly2019/SortingAndSearchingAlgorithmsExercises/04.Needles/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Needles
+{
+
+    class Program
+    {
+        static List<int> indices = new List<int>();
+
+        static void Main(string[] args)
+        {
+            int[] cn = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] spots = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] needles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            foreach (var needle in needles)
+            {
+                bool match = false;
+
+                for (int i = 0; i < spots.Length; i++)
+                {
+                    if (spots[i] >= needle)
+                    {
+                        match = true;
+                        ReturnIndex(spots, i - 1);
+                        break;
+                    }
+
+                }
+
+                if (match==false)
+                {
+                    ReturnIndex(spots, spots.Length - 1);
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", indices));
+
+
+        }
+
+        private static void ReturnIndex(int[] spots, int i)
+        {
+
+            while (i >= 0)
+            {
+                if (spots[i] != 0)
+                {
+                    indices.Add(i + 1);
+                    return;
+                }
+
+                i--;
+            }
+            indices.Add(0);
+           
+        }
+    }
+}
+
---
+++ SoftUniAlgorithmsJuly2019/SortingAndSearchingAlgorithmsExercises/04.Words/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Words
+{
+    class Program
+    {
+        static int count = 0;
+        static char[] word;
+        static void Main(string[] args)
+        {
+            word = Console.ReadLine().ToCharArray();
+            int wordDistinctCharacters = word.Distinct().Count();
+            
+            if (wordDistinctCharacters == word.Length)
+            {
+                count = Fact(wordDistinctCharacters);
+            }
+            else
+            {
+                WordPermutations(0);
+            }
+            Console.WriteLine(count);
+        }
+
+        private static int Fact(int wordDistinctCharacters)
+        {
+            int fact = 1;
+            for (int i = 2; i <= wordDistinctCharacters; i++)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+
+        private static void WordPermutations(int index)
+        {
+            if (index == word.Length)
+            {
+                bool isValid = true;
+
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    if (word[i] == word[i + 1])
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (isValid)
+                {
+                    count++;
+                }
+
+                return;
+            }
+
+            var usedLetters = new HashSet<char>();
+
+            for (int i = index; i < word.Length; i++)
+            {
+                if (!usedLetters.Contains(word[i]))
+                {
+                    char currentChar = word[index];
+                    word[index] = word[i];
+                    word[i] = currentChar;
+
+                    WordPermutations(index + 1);
+
+                    usedLetters.Add(word[index]);
+
+                    word[i] = word[index];
+                    word[index] = currentChar;
+                }
+
+
+
+            }
+        }
+    }
+}
