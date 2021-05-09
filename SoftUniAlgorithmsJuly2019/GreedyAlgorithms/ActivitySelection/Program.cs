79b8f0e5c402b47322181845eb67c6321f644e43
iwanmitowski <iwan.mitowski@gmail.com>
2021-05-09 09:39:56 +0300

    Greedy Algorithms lab

---
+++ SoftUniAlgorithmsJuly2019/GreedyAlgorithms/ActivitySelection/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActivitySelection
+{
+    class Activity
+    {
+        public Activity(int start, int finish)
+        {
+            Start = start;
+            Finish = finish;
+        }
+
+        public int Start { get; }
+        public int Finish { get; }
+
+        public override string ToString()
+        {
+            return $"{this.Start} - {this.Finish}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Activity> activities = new List<Activity>();
+
+            int[] startTimes = new int[] { 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12 };
+            int[] finishTimes = new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+            for (int i = 0; i < startTimes.Length; i++)
+            {
+                activities.Add(new Activity(startTimes[i], finishTimes[i]));
+            }
+
+            var last = activities
+                .OrderBy(x => x.Finish)
+                .First();
+
+            Console.WriteLine(last);
+
+            foreach (var a in activities)
+            {
+                if (a.Start>=last.Finish)
+                {
+                    last = a;
+                    Console.WriteLine(a);
+                }
+            }
+        }
+    }
+}
---
+++ SoftUniAlgorithmsJuly2019/GreedyAlgorithms/SetCover/SetCover.cs
@@ -0,0 +1,59 @@
+﻿namespace SetCover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SetCover
+    {
+        public static void Main(string[] args)
+        {
+            var universe = new[] { 1, 3, 5, 7, 9, 11, 20, 30, 40 };
+            var sets = new[]
+            {
+                new[] { 20 },
+                new[] { 1, 5, 20, 30 },
+                new[] { 3, 7, 20, 30, 40 },
+                new[] { 9, 30 },
+                new[] { 11, 20, 30, 40 },
+                new[] { 3, 7, 40 }
+            };
+
+            var selectedSets = ChooseSets(sets.ToList(), universe.ToList());
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+            foreach (var set in selectedSets)
+            {
+                Console.WriteLine($"{{ {string.Join(", ", set)} }}");
+            }
+        }
+
+        public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+        {
+            var result = new List<int[]>();
+
+            var universeSet = new HashSet<int>(universe);
+            var setsSet = new HashSet<int[]>(sets);
+
+            while (universeSet.Count>0)
+            {
+
+                // Take me the first which contains the most elements from the other set/collection
+
+                var currentSet = setsSet
+                    .OrderByDescending(set=>set.Count(item=>universeSet.Contains(item)))
+                    .First();
+
+                foreach (var item in currentSet)
+                {
+                    universeSet.Remove(item);
+                }
+
+                sets.Remove(currentSet);
+
+                result.Add(currentSet);
+            }
+
+            return result;
+        }
+    }
+}
