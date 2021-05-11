c133877c0eb5ce6060f08edc84283b492a73a77f
iwanmitowski <iwan.mitowski@gmail.com>
2021-05-11 17:07:44 +0300

    Last exercises from Greedy Algorithms

---
+++ SoftUniAlgorithmsJuly2019/GreedyAlgorithmsExercise/04.BestLecturesShcedule/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BestLecturesShcedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lecturesCount = int.Parse(Console.ReadLine().Split().ToArray()[1]);
+
+            List<Lecture> allLectures = new List<Lecture>();
+
+            for (int i = 0; i < lecturesCount; i++)
+            {
+                string[] lectureTokens = Console.ReadLine().Split().ToArray();
+
+                string name = lectureTokens[0];
+                
+                int s = int.Parse(lectureTokens[1]);
+                int f = int.Parse(lectureTokens[3]);
+                
+
+                Lecture lecture = new Lecture(name, s, f);
+
+                allLectures.Add(lecture);
+            }
+
+            var last = allLectures.OrderBy(x => x.Finish).FirstOrDefault();
+
+            List<Lecture> visited = new List<Lecture>();
+            visited.Add(last);
+
+            foreach (var l in allLectures)
+            {
+                if (l.Start>last.Finish)
+                {
+                    last = l;
+                    visited.Add(last);
+                }
+            }
+
+            Console.WriteLine($"Lectures {visited.Count}");
+
+            foreach (var v in visited)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+    class Lecture
+    {
+        public Lecture(string name, int start, int finish)
+        {
+            Name = name;
+            Start = start;
+            Finish = finish;
+        }
+
+        public string Name { get; }
+        public int Start { get;}
+        public int Finish { get;}
+
+        public override string ToString()
+        {
+            return $"{this.Start}-{this.Finish} -> {this.Name}";
+        }
+    }
+}
---
+++ SoftUniAlgorithmsJuly2019/GreedyAlgorithmsExercise/05.EgiptianFraction/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.EgiptianFraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split("/").Select(int.Parse).ToArray();
+
+            int p = numbers[0];
+            int q = numbers[1];
+
+            Console.Write($"{p}/{q} = ");
+
+            List<string> realNumbers = new List<string>();
+
+            if (p>=q)
+            {
+                Console.WriteLine($"Error (fraction is equal to or greater than 1)");
+                Environment.Exit(0);
+            }
+
+            if (q%p==0)
+            {
+                q = q / p;
+               
+                realNumbers.Add($"1/{q}");
+
+                Console.WriteLine(string.Join(" + ", realNumbers));
+                Environment.Exit(0);
+            }
+
+            while (true)
+            {
+                int denominator = (p + q) / p;
+
+                realNumbers.Add($"1/{denominator}");
+
+                p = p*denominator-q;
+                q *=denominator;
+
+                if (q % p == 0)
+                {
+                    q = q / p;
+                   
+                    realNumbers.Add($"1/{q}");
+                    break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" + ", realNumbers));
+        }
+    }
+}
