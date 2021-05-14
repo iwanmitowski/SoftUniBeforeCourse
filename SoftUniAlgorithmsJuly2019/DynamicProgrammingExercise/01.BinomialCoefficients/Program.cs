c449e5f81a4b2f8c7abcfd6fc6f3c48efcb56584
iwanmitowski <iwan.mitowski@gmail.com>
2021-05-14 15:20:50 +0300

    2/5 Dynamic Programming Exercises

---
+++ SoftUniAlgorithmsJuly2019/DynamicProgrammingExercise/01.BinomialCoefficients/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01.BinomialCoefficients
+{
+    class Program
+    {
+        static long[,] binoms;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            binoms = new long[n + 1, k + 1];
+
+            long binom = Binomial(n, k);
+
+            Console.WriteLine(binom);
+        }
+
+        private static long Binomial(int n, int k)
+        {
+            if (binoms[n,k]!=0)
+            {
+                return binoms[n, k];
+            }
+
+            if (k > n)
+            {
+                return 0;
+            }
+
+            if (k == n || k == 0)
+            {
+                binoms[n, k] = 1;
+            }
+            else
+            {
+                binoms[n, k] = Binomial(n - 1, k - 1) + Binomial(n - 1, k);
+            }
+
+            return binoms[n, k];
+
+        }
+    }
+}
---
+++ SoftUniAlgorithmsJuly2019/DynamicProgrammingExercise/03.DividingPresents/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.DividingPresents
+{
+    class Program
+    {
+        static int[] presents;
+        static int[] indexes;
+        static void Main(string[] args)
+        {
+            presents = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int total = presents.Sum();
+
+            indexes = new int[total + 1];
+
+            for (int i = 1; i < total + 1; i++)
+            {
+                indexes[i] = -1;
+            }
+
+
+            for (int i = 0; i < presents.Length; i++)
+            {
+                for (int j = total; j >= 0; j--)
+                {
+                    if (indexes[j] != -1 && indexes[j + presents[i]] == -1)
+                    {
+                        indexes[j + presents[i]] = i;
+                    }
+                }
+            }
+
+            int half = total / 2;
+
+            for (int i = half; i >= 0; i--)
+            {
+                if (indexes[i] != -1)
+                {
+                    int bobShare = total - i;
+                    int alanShare = i;
+                    int difference = bobShare - alanShare;
+
+                    Console.WriteLine($"Difference: {difference}");
+                    Console.WriteLine($"Alan:{alanShare} Bob:{bobShare}");
+                    Console.WriteLine($"Alan takes: {GetAlanPresents(alanShare)}");
+                    Console.WriteLine($"Bob takes the rest.");
+                    break;
+                }
+            }
+        }
+
+        private static string GetAlanPresents(int alanShare)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (alanShare!=0)
+            {
+                int currentPressent = presents[indexes[alanShare]];
+                sb.Append($"{currentPressent} ");
+                alanShare -= currentPressent;
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
