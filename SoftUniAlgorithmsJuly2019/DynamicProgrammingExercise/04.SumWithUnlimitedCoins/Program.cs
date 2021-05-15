4f2b8a9647e754369822798b7e156901d68b7629
iwanmitowski <iwan.mitowski@gmail.com>
2021-05-15 11:43:38 +0300

    Adding 2 more tasks

---
+++ SoftUniAlgorithmsJuly2019/DynamicProgrammingExercise/04.SumWithUnlimitedCoins/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SumWithUnlimitedCoins
+{
+    class Program
+    {
+        static int targetSum;
+        static int[] coins;
+        static int totalSums;
+        static void Main(string[] args)
+        {
+            coins = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            targetSum = int.Parse(Console.ReadLine());
+
+
+            CalculatePossibleSums(0, 0);
+
+            Console.WriteLine(totalSums);
+            
+        }
+        static void CalculatePossibleSums(int sum, int start)
+        {
+            if (sum == targetSum)
+            {
+                totalSums++;
+                return;
+            }
+            if (sum > targetSum)
+            {
+                return;
+            }
+
+            for (int i = start; i < coins.Length; i++)
+            {
+                CalculatePossibleSums(sum + coins[i], i);
+            }
+        }
+    }
+}
---
+++ SoftUniAlgorithmsJuly2019/DynamicProgrammingExercise/05.SumWithLimitedCoins/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SumWithLimitedCoins
+{
+    class Program
+    {
+        static int targetSum;
+        static int[] coins;
+        static int totalSums;
+        static List<int> currentCombination = new List<int>();
+        static HashSet<string> combinations = new HashSet<string>();
+        static void Main(string[] args)
+        {
+            coins = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            targetSum = int.Parse(Console.ReadLine());
+
+            CalculatePossibleSums(0, 0);
+
+            Console.WriteLine(totalSums);
+
+        }
+        static void CalculatePossibleSums(int sum, int start)
+        {
+            if (sum == targetSum)
+            {
+                string combination = string.Join(" ",currentCombination.OrderBy(x=>x));
+
+                if (combinations.Contains(combination))
+                {
+                    return;
+                }
+
+                combinations.Add(combination);
+                totalSums++;
+
+                return;
+            }
+
+            if (sum > targetSum)
+            {
+                return;
+            }
+
+            for (int i = start; i < coins.Length; i++)
+            {
+                currentCombination.Add(coins[i]);
+                CalculatePossibleSums(sum + coins[i], i+1);
+                currentCombination.Remove(coins[i]);
+            }
+        }
+    }
+}
