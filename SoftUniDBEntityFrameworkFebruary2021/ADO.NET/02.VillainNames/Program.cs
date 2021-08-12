db43dc568cc67567614f52c8b28d43a2ff144047
iwanmitowski <iwan.mitowski@gmail.com>
2021-08-12 20:43:52 +0300

    Adding some of the ADO.NET exercises

---
+++ SoftUniDBEntityFrameworkFebruary2021/ADO.NET/02.VillainNames/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _02.VillainNames
+{
+    class Program
+    {
+        const string connectionString = "Server=.;Database=MinionsDB;Integrated Security=true;";
+
+        static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var query = @"
+  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+    FROM Villains AS v 
+    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+GROUP BY v.Id, v.Name 
+  HAVING COUNT(mv.VillainId) > 3 
+ORDER BY COUNT(mv.VillainId) DESC";
+
+            using var command = new SqlCommand(query, connection);
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader[0]} - {reader[1]}");
+            }
+        }
+    }
+}
---
+++ SoftUniDBEntityFrameworkFebruary2021/ADO.NET/03.MinionNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _03.MinionNames
+{
+    class Program
+    {
+        const string connectionString = "Server=.;Database=MinionsDB;Integrated Security=true;";
+
+        static void Main(string[] args)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string[] minionData = Console.ReadLine().Split();
+
+            string minionName = minionData[1];
+            int age = int.Parse(minionData[2]);
+            string town = minionData[3];
+
+
+        }
+    }
+}
