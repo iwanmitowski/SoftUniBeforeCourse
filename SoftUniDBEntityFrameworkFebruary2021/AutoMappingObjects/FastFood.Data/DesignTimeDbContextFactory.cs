46fc53822ce1c22b55b13d46c66188b380d66029
iwanmitowski <iwan.mitowski@gmail.com>
2021-09-06 11:48:22 +0300

    Setting up AutoMapper and DesignTimeDbContextFactory

---
+++ SoftUniDBEntityFrameworkFebruary2021/AutoMappingObjects/FastFood.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FastFood.Data
+{
+    class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            //requires Microsoft.Extensions.Configuration.Json 
+            //and appsettings.json with DefaultConnection in it
+
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory()) 
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
